2718e6274891f218adfc2e97fbc2f7d8afefe580
HardikJain-Harry <hjain2734@gmail.com>
2023-09-08 23:38:01 +0530

    initial commit

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>airbnb 2.0</title>
+        
+      </Head>
+      <Header />
+    </div>
+  )
+}
